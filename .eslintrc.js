2742fbdbb626f797b43be3860c64b64fe9f2786a
NoealRajeev <noealrajeev987@gmail.com>
2025-07-24 17:24:10 +0530

    last ayitu nokam

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    // if you really want to disable no-explicit-any, keep this:
+    "@typescript-eslint/no-explicit-any": "off",
+    // … your other rules …
+  },
+};
